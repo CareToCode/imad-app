9bd3aee876db1b328237fb78eac8f167edbcfc46
CareToCode <31237295+CareToCode@users.noreply.github.com>
2017-08-26 12:21:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,46 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne= {
+    title: 'Article One',
+    heading: 'Article One',
+    content : 'Look what you made me do.'
+    
+};
+
+function createTemplate(data){
+    var title=data.title;
+    var heading=data.heading;
+    var content = data.content;
+var htmlTemplate = ` 
+<html>
+    <head>
+        <title>${title}</title>
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="container">
+            <a href="/">Home</a>
+        
+        <hr>
+        
+        <h3>${heading}</h3>
+        
+        
+            <p>${content}</p>
+        </div>
+</html>
+
+`;
+return html
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    res.send(createTemplate(articleOne));
     
 });
 app.get('/article-two',function(req,res){
